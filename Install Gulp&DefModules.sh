be7bf80f5ed102457940ef7c9e28489a9a957435
Eduard <102860662+Hajime-in-heart@users.noreply.github.com>
2022-12-13 01:15:53 +0200

    Add files via upload

---
+++ Install Gulp&DefModules.sh
@@ -0,0 +1,2 @@
+npm i gulp browser-sync gulp-concat gulp-uglify-es gulp-sass gulp-newer jquery gulp-less gulp-autoprefixer gulp-clean-css compress-images gifsicle@5.3.0 pngquant-bin@6.0.0 gulp-clean --save-dev
+npm i -D sass
