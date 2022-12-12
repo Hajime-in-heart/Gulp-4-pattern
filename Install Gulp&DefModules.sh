559bbb0f3c6d7cfe3d1d6356aefaf17a73c23d1b
Eduard <102860662+Hajime-in-heart@users.noreply.github.com>
2022-12-13 01:41:30 +0200

    Delete Install Gulp&DefModules.sh

--- Install Gulp&DefModules.sh
+++
@@ -1,2 +0,0 @@
-npm i gulp browser-sync gulp-concat gulp-uglify-es gulp-sass gulp-newer jquery gulp-less gulp-autoprefixer gulp-clean-css compress-images gifsicle@5.3.0 pngquant-bin@6.0.0 gulp-clean --save-dev
-npm i -D sass
